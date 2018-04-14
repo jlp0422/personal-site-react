82d4002ddc2c667779e738ce1abee499168cfe95
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-14 17:24:36 -0400

    first commit server set up

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <h1>This is my site</h1>
+  )
+}
+
+export default App;
