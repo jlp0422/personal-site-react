5b8153aa7faf35cc2442055062d3e4e1e5798a99
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-15 21:29:14 -0400

    nav should be just about done

--- src/Nav.js
+++ src/Nav.js
@@ -3,24 +3,51 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  const url = location.hash.slice(1)
   return (
     <div>
       <nav className="navbar sticky-top navbar-light bg-light">
         <div className="container">
           <Link className="navbar-brand" to='/home'>
-            <img style={{width: '30px', height: '30px'}} src="./vendor/images/logo-black-no-bkg.png" className="d-inline-block align-middle" />
-            &nbsp;Home
+            <img style={{width: '30px', marginTop: '-15px' }} src="./vendor/images/logo-black-no-bkg.png" className="d-inline-block align-middle" />
           </Link>
           <ul className="nav">
-            <li style={{margin: '0px 5px'}} className="nav-item">
-              <Link to='/about'>About</Link>
-            </li>
-            <li style={{margin: '0px 5px'}} className="nav-item">
-              <Link to='/experience'>Experience</Link>
-            </li>
-            <li style={{margin: '0px 5px'}} className="nav-item">
-              <Link to='/resume'>Resume</Link>
-            </li>
+            {
+              url === '/home' ? (
+                <span className="nav-item nav-page-selected">Home</span>
+              ) : (
+              <li className="nav-item nav-page-link">
+                <Link to='/home'>Home</Link>
+              </li>
+              )
+            }
+            {
+              url === '/about' ? (
+                <span className="nav-item nav-page-selected">About</span>
+              ) : (
+              <li className="nav-item nav-page-link">
+                <Link to='/about'>About</Link>
+              </li>
+              )
+            }
+            {
+              url === '/experience' ? (
+                <span className="nav-item nav-page-selected">Experience</span>
+              ) : (
+              <li className="nav-item nav-page-link">
+                <Link to='/experience'>Experience</Link>
+              </li>
+              )
+            }
+            {
+              url === '/resume' ? (
+                <span className="nav-item nav-page-selected">Resume</span>
+              ) : (
+              <li className="nav-item nav-page-link">
+                <Link to='/resume'>Resume</Link>
+              </li>
+              )
+            }
           </ul>
         </div>
       </nav>
