2d7ca73e24fdafebbd5cb50e7d2a429bb6bc1b54
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-17 18:42:41 -0400

    trying to fix experience page

--- src/Experience.js
+++ src/Experience.js
@@ -7,11 +7,11 @@ const Experience = () => {
     <div className="margin-default">
     <Helmet><title>Experience | Jeremy Philipson</title></Helmet>
       <div className="flex exp-card-container flex-column space-around">
-        <div className="flex space-around exp-card">
+        <div className="flex exp-card">
           <div>
             <img style={{height: '200px'}}src="./vendor/images/fanduel_logo.png" />
           </div>
-          <div className="flex flex-colum">
+          <div className="flex flex-column">
             <div>
               <h2>Email Marketing Associate</h2>
               <h3>FanDuel</h3>
@@ -20,24 +20,48 @@ const Experience = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-column exp-card">
-          <h2>Email Marketing Coordinator</h2>
-          <h3>FanDuel</h3>
-          <h5>June 2016 &ndash; February 2017</h5>
-          <h5>New York, NY</h5>
+
+        <div className="flex space-around exp-card">
+          <div>
+            <img style={{ height: '200px' }} src="./vendor/images/fanduel_logo.png" />
+          </div>
+          <div className="flex flex-column">
+            <div>
+              <h2>Email Marketing Coordinator</h2>
+              <h3>FanDuel</h3>
+              <h5>June 2016 &ndash; February 2017</h5>
+              <h5>New York, NY</h5>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-column exp-card">
-          <h2>Customer Support Agemt</h2>
-          <h3>FanDuel</h3>
-          <h5>June 2015 &ndash; June 2016</h5>
-          <h5>New York, NY</h5>
+        <div className="flex space-around exp-card">
+          <div>
+            <img style={{ height: '200px' }} src="./vendor/images/fanduel_logo.png" />
+          </div>
+          <div className="flex flex-column">
+            <div>
+              <h2>Customer Support Agent</h2>
+              <h3>FanDuel</h3>
+              <h5>June 2015 &ndash; June 2016</h5>
+              <h5>New York, NY</h5>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-column exp-card">
-          <h2>Sports Marketing Coordinator</h2>
-          <h3>Wake Forest University</h3>
-          <h5>September 2013 &ndash; June 2015</h5>
-          <h5>Winston Salem, NC</h5>
+
+        <div className="flex space-around exp-card">
+          <div>
+            <img style={{ height: '200px' }} src="./vendor/images/fanduel_logo.png" />
+          </div>
+          <div className="flex flex-column">
+            <div>
+              <h2>Sports Marketing Coordinator</h2>
+              <h3>Wake Forest University</h3>
+              <h5>September 2013 &ndash; June 2015</h5>
+              <h5>Winston Salem, NC</h5>
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
   )
